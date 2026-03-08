e9d893afbbd46847cdea5b33c37d1f1756b1b97e
Elmer Marquez <leomarqz.main@gmail.com>
2026-03-08 01:55:35 -0600

    Se realizaron los registros de los servicios y paquetes necesarios para el uso de Authentication con identity

--- TechNotes.Infrastructure/DependencyInjection.cs
+++ TechNotes.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,14 @@
 
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using TechNotes.Application.Authentication;
 using TechNotes.Domain.Notes;
+using TechNotes.Infrastructure.Authentication;
 using TechNotes.Infrastructure.Repositories;
 
 namespace TechNotes.Infrastructure;
@@ -21,6 +27,25 @@ public static class DependencyInjection
 
         services.AddScoped<INoteRepository, NoteRepository>();
         
+        AddAuthentication(services);
+        
         return services;
+    }
+
+    private static void AddAuthentication(IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+        services.AddCascadingAuthenticationState();
+        services.AddAuthorization();
+        services.AddAuthentication((options) =>
+        {
+            options.DefaultScheme = IdentityConstants.ApplicationScheme;
+            options.DefaultChallengeScheme = IdentityConstants.ExternalScheme;
+        }).AddIdentityCookies();
+        services.AddIdentityCore<User>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
     }
 }
