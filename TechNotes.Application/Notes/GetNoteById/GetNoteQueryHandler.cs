9a8e7d805d0647168a545d98119b985e98a3dbd5
Elmer Marquez <leomarqz.main@gmail.com>
2026-03-14 14:45:17 -0600

    Se agrego el username del creador de una nota en la vista de NoteEditor en procesos de Edicion y Actualizacion de una nota

--- TechNotes.Application/Notes/GetNoteById/GetNoteQueryHandler.cs
+++ TechNotes.Application/Notes/GetNoteById/GetNoteQueryHandler.cs
@@ -1,13 +1,17 @@
 
+using TechNotes.Domain.User;
+
 namespace TechNotes.Application.Notes.GetNoteById;
 
 public class GetNoteQueryHandler : IQueryHandler<GetNoteByIdQuery, NoteResponse?>
 {
     private readonly INoteRepository _noteRepository;
+    private readonly IUserRepository _userRepository;
 
-    public GetNoteQueryHandler(INoteRepository noteRepository)
+    public GetNoteQueryHandler(INoteRepository noteRepository, IUserRepository userRepository)
     {
         _noteRepository = noteRepository;
+        _userRepository = userRepository;
     }
 
     public async Task<Result<NoteResponse?>> Handle(GetNoteByIdQuery request, CancellationToken cancellationToken)
@@ -17,6 +21,18 @@ public class GetNoteQueryHandler : IQueryHandler<GetNoteByIdQuery, NoteResponse?
         if( note is null )
             return Result.Fail<NoteResponse?>("Nota no encontrada!");
 
-        return note.Adapt<NoteResponse>();
+        var noteResponse = note.Adapt<NoteResponse>();
+
+        if( note.UserId != null)
+        {
+           var noteAuthor = await _userRepository.GetUserByIdAsync( note.UserId! );
+           noteResponse.UserName = noteAuthor?.UserName ?? "Desconocido";
+        }
+        else
+        {
+            noteResponse.UserName = "Desconocido";
+        }
+
+        return noteResponse;
     }
 }
